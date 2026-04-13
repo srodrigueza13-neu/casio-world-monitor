d49a0ea2e8502dd34da6263fcd43db2ad346976b
srodrigueza13-neu <srodrigueza13@gmail.com>
2026-04-12 22:44:24 -0400

    Update weather.js

--- api/weather.js
+++ api/weather.js
@@ -1,7 +1,37 @@
+const https = require("https");
+
+function getJson(url) {
+return new Promise((resolve, reject) => {
+https
+.get(url, (res) => {
+let data = "";
+
+```
+    res.on("data", (chunk) => {
+      data += chunk;
+    });
+
+    res.on("end", () => {
+      try {
+        resolve(JSON.parse(data));
+      } catch (err) {
+        reject(err);
+      }
+    });
+  })
+  .on("error", (err) => {
+    reject(err);
+  });
+```
+
+});
+}
+
 module.exports = async function (req, res) {
 try {
-const r = await fetch("https://api.open-meteo.com/v1/forecast?latitude=-34.58&longitude=-70.99&current=temperature_2m");
-const data = await r.json();
+const data = await getJson(
+"https://api.open-meteo.com/v1/forecast?latitude=-34.58&longitude=-70.99&current=temperature_2m"
+);
 
 ```
 res.status(200).json({
